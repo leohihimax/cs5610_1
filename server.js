e6109f52397bbc3c7fc64c636572da6bcfffea4c
leohihimax <leohihimax@gmail.com>
2014-10-12 22:09:21 -0400

    fix app

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ if(typeof process.env.OPENSHIFT_MONGODB_DB_URL == "undefined"){
 
 var db = mongojs("mongodbConnectionString", ["serviceClients"]);
 
-ipaddress = process.env.OPENSHIFT_NODEJS_IP ||"127.0.0.1";
-port      = process.env.OPENSHIFT_NODEJS_PORT || 3000; 
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP ||"127.0.0.1";
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000; 
 
 app.get("/env", function(req, res){
     res.json(process.env);
@@ -61,6 +61,6 @@ app.put("/serviceClients/:id", function (req, res) {
 
 });
 
-app.listen(3000);
+
 
 app.listen(port, ipaddress);
