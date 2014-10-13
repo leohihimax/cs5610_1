db3f49a0b98451a35d0ef52f8cc69d6d5bd394b6
leohihimax <leohihimax@gmail.com>
2014-10-12 21:46:41 -0400

    add mongojs

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require("express");
-//var mongojs = require("mongojs");
+var mongojs = require("mongojs");
 var app = express();
-//var db = mongojs("fisrt", ["serviceClients"]);
 
+var mongodbConnectionString = process.env.OPENSHIFT_MONGODB_DB_URL + "applications";
+
+if(typeof process.env.OPENSHIFT_MONGODB_DB_URL == "undefined"){
+    mongodbConnectionString = "applications";
+}
+
+var db = mongojs("mongodbConnectionString", ["serviceClients"]);
 
 ipaddress = process.env.OPENSHIFT_NODEJS_IP ||"127.0.0.1";
 port      = process.env.OPENSHIFT_NODEJS_PORT || 3000; 
